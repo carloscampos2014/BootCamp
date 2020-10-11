b2304308a1df80fa11dde21dae0ce0ad1d13f32b
carloscampos2014 <carlosalbertode.campos@gmail.com>
2020-10-11 20:13:45 -0300

    Middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,46 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { v1, validate } = require("uuid");
 
 let projects = [];
+
+const logRequest = (req, res, next) => {
+  const { method, url } = req;
+  const logLabel = `${method.toUpperCase()} ${url}`
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+
+const validateProjectId = (req, res, next) => {
+  const { id } = req.params;
+  if (validate(id) === false) return res.status(404).send("<p>ID Invalido!</p>");
+  else return next()
+}
 
 const app = express();
 
 app.use(express.json());
 
+app.use(logRequest);
+app.use("/projects/:id", validateProjectId);
+
 app.get("/projects", (req, res) => {
-  return res.status(200).json(projects);
+  const { title } = req.query;
+  const result = title ? projects.filter((p) => p.title.toLowerCase().includes(title.toLowerCase())) : projects;
+  return res.status(200).json(result);
 });
 
 app.get("/projects/:id", (req, res) => {
   const { id } = req.params;
-  return res.status(200).json(projects[id - 1]);
+  const position = projects.findIndex((project) => project.id === id);
+  if (position < 0)
+    return res.status(404).send("<p>Projeto não Encontrado!</p>");
+  return res.status(200).json(projects[position]);
 });
 
 app.post("/projects", (req, res) => {
   const { title, owner } = req.body;
-  const id = uuid();
-  const project = { id, title, owner };
+  const project = { id: v1(), title, owner };
   projects.push(project);
   return res.status(200).json(project);
 });
